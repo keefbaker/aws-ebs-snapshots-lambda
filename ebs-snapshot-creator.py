68d5bc60689fd0c333b04e74c36722209473299d
Keith Baker <keith.baker@checkit.net>
2018-01-26 14:01:05 +0000

    cleanup

--- ebs-snapshot-creator.py
+++ ebs-snapshot-creator.py
@@ -1,15 +1,72 @@
-import boto3
+from __future__ import print_function
+import os
 import collections
 import datetime
+import boto3
 
-region = 'us-west-2'    # region we're running in (should be changed to be auto-determined 
+region = os.environ.get("AWS_REGION", "eu-west-1")
 
 ec = boto3.client('ec2')
 
-def lambda_handler(event, context):
+def instance_names(instance):
+    for tag in instance['Tags']:
+        if tag['Key'] == 'Name':
+            i_name = tag['Value']
+            break
+    else:
+        i_name = ""
+    return i_name
+
+
+def device_process(dev, to_tag, instance, retention_days):
+    if dev.get('Ebs', None) is None:
+        return to_tag
+    vol_id = dev['Ebs']['VolumeId']
+    dev_name = dev['DeviceName']
+
+    instance_name = instance_names(instance)
+    description = '%s - %s (%s)' % (instance_name, vol_id, dev_name)
+
+    snap = ec.create_snapshot(
+        VolumeId=vol_id,
+        Description=description
+        )
+
+    if snap:
+        print("Snapshot {} created in {} of [{}]".format(snap['SnapshotId'],
+                                                         region,
+                                                         description))
+    to_tag[retention_days].append(snap['SnapshotId'])
+    return to_tag
+
+def instance_process(instance, to_tag):
+    try:
+        retention_days = [
+            int(tag.get('Value')) \
+            for tag in instance['Tags']
+            if tag['Key'] == 'Retention'
+            ][0]
+    except IndexError:
+        retention_days = 7
+
+    for dev in instance['BlockDeviceMappings']:
+        to_tag = device_process(dev, to_tag, instance, retention_days)
+    return to_tag
+
+def instance_loop(instances):
+    to_tag = collections.defaultdict(list)
+
+    for instance in instances:
+        to_tag = instance_process(instance, to_tag)
+    return to_tag
+
+def lambda_handler(*args):
     reservations = ec.describe_instances(
         Filters=[
-            { 'Name': 'tag:Backup', 'Values': ['Yes'] },
+            {
+                'Name': 'tag:Backup',
+                'Values': ['true']
+            },
         ]
     ).get(
         'Reservations', []
@@ -17,65 +74,29 @@ def lambda_handler(event, context):
 
     instances = sum(
         [
-            [i for i in r['Instances']]
-            for r in reservations
+            [instance for instance in reservation['Instances']]
+            for reservation in reservations
         ], [])
 
-    print "Found %d instances that need backing up" % len(instances)
+    print("Found {} instances that need backing up".format(len(instances)))
 
-    to_tag = collections.defaultdict(list)
-
-    for instance in instances:
-        try:
-            retention_days = [
-                int(t.get('Value')) for t in instance['Tags']
-                if t['Key'] == 'Retention'][0]
-        except IndexError:
-            retention_days = 7
-
-        for dev in instance['BlockDeviceMappings']:
-            if dev.get('Ebs', None) is None:
-                continue
-            vol_id = dev['Ebs']['VolumeId']
-            dev_name = dev['DeviceName']
-            print "\tFound EBS volume %s (%s) on instance %s" % (
-                vol_id, dev_name, instance['InstanceId'])
-
-            # figure out instance name if there is one
-            instance_name = ""
-            for tag in instance['Tags']:
-                if tag['Key'] != 'Name':
-                    continue
-                else:
-                    instance_name = tag['Value']
-            
-            description = '%s - %s (%s)' % ( instance_name, vol_id, dev_name )
-
-            # trigger snapshot
-            snap = ec.create_snapshot(
-                VolumeId=vol_id, 
-                Description=description
-                )
-            
-            if (snap):
-                print "\t\tSnapshot %s created in %s of [%s]" % ( snap['SnapshotId'], region, description )
-            to_tag[retention_days].append(snap['SnapshotId'])
-            print "\t\tRetaining snapshot %s of volume %s from instance %s (%s) for %d days" % (
-                snap['SnapshotId'],
-                vol_id,
-                instance['InstanceId'],
-                instance_name,
-                retention_days,
-            )
+    to_tag = instance_loop(instances)
 
     for retention_days in to_tag.keys():
-        delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+        delete_date = datetime.date.today() + datetime.timedelta(
+            days=retention_days
+            )
         delete_fmt = delete_date.strftime('%Y-%m-%d')
-        print "Will delete %d snapshots on %s" % (len(to_tag[retention_days]), delete_fmt)
+        print("Will delete {} snapshots on {}".format(
+            len(to_tag[retention_days]),
+            delete_fmt))
         ec.create_tags(
             Resources=to_tag[retention_days],
             Tags=[
-                { 'Key': 'DeleteOn', 'Value': delete_fmt },
-                { 'Key': 'Type', 'Value': 'Automated' },
+                {'Key': 'DeleteOn', 'Value': delete_fmt},
+                {'Key': 'Type', 'Value': 'Automated'},
             ]
         )
+
+if __name__ == "__main__":
+    lambda_handler("hello", "world")
